ca6af8de66196e536512b6c67ce1478c648036df
Siva Kumaran <r.sivakumaran@contractor.temenos.com>
2022-05-12 10:46:56 +0530

    title change

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -4,25 +4,44 @@ export default function ExportSectionHeader(props) {
 
   if (!props.empty) {
     if (props.operation === "category" || props.operation === "init") {
-      if (props.value.toLowerCase() === 'top'){
-        content = "Top Stories"
-      }else{
-        content = props.value
+      if (props.value.toLowerCase() === "top") {
+        content = "Top Stories";
+      } else {
+        content = props.value;
       }
-    } else if (props.operation === "search" || (props.operation === 'pagination' && localStorage.getItem('lastReq') === 'search')) {
-      content = <>Showing results for <span style={{color: 'var(--primary)', textTransform: 'capitalize'}}>{props.value}</span></>;
-    } else if (props.operation === "language" || (props.operation === 'pagination' && localStorage.getItem('lastReq') === 'language')) {
+    } else if (
+      props.operation === "search" ||
+      (props.operation === "pagination" &&
+        localStorage.getItem("lastReq") === "search")
+    ) {
+      content = (
+        <>
+          Showing results for{" "}
+          <span
+            style={{ color: "var(--primary)", textTransform: "capitalize" }}
+          >
+            {props.value}
+          </span>
+        </>
+      );
+    } else if (
+      props.operation === "language" ||
+      (props.operation === "pagination" &&
+        localStorage.getItem("lastReq") === "language")
+    ) {
       content = "Top Stories";
-    }
-    else if (props.operation === 'pagination'){
+    } else if (props.operation === "pagination") {
       content = props.value;
     }
-  }else{
+  } else {
     flag = true;
+  }
+  if (props.value.toLowerCase() === "top") {
+    content = "Top Stories";
   }
 
   return (
-    <div className="section-head" style={{display: !flag? 'flex' : 'none' }}>
+    <div className="section-head" style={{ display: !flag ? "flex" : "none" }}>
       <h1>{content}</h1>
     </div>
   );
